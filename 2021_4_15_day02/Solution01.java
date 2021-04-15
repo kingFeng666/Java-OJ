49e1193103e9602404a5e1f2bc33d80aa89c7b4b
kingFeng666 <1072784590@qq.com>
2021-04-15 18:56:30 +0800

    第一只出现一次的字符

---
+++ 2021_4_15_day02/Solution01.java
@@ -0,0 +1,20 @@
+public class Solution01 {
+    public int FirstNotRepeatingChar(String str) {
+        if (str.length() == 0 || str == " ") {
+            return -1;
+        }
+        char[] chars = str.toCharArray();
+        char[] temp = new char[256];
+        for(int i = 0 ; i < chars.length; i++) {
+            //字符的ASCLL 作为数组的下标  出现的次数作为小标对应的值
+            temp[chars[i]]++;
+        }
+        for (int i = 0 ; i < temp.length; i++ ) {
+            //要按照字符串的字符顺序进行遍历
+            if(temp[chars[i]] == 1 ) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
