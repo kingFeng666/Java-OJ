20abacef54681f8c51022f7e80982b882ef26ddb
kingFeng666 <1072784590@qq.com>
2021-05-15 23:56:47 +0800

    剑指offer

--- 2021_5_15_day22/Soluction.java
+++ 2021_5_15_day22/Soluction.java
@@ -14,10 +14,6 @@
   2   2
    \   \
    3    3
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
