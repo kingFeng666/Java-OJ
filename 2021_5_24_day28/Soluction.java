8c43f800bfa487e3d960cff3c4b66e8c8268251b
kingFeng666 <1072784590@qq.com>
2021-05-26 21:31:54 +0800

    剑指 Offer 27. 二叉树的镜像

---
+++ 2021_5_24_day28/Soluction.java
@@ -0,0 +1,37 @@
+/**
+输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+示例 1：
+输入：arr = [3,2,1], k = 2
+输出：[1,2] 或者 [2,1]
+示例 2：
+输入：arr = [0,1,2,1], k = 1
+输出：[0]
+*/
+public int[] getLeastNumbers(int[] arr, int k) {
+        //top k 问题
+        int[] vec = new int[k];
+        
+        if (k == 0) { // 排除 0 的情况
+            return vec;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            queue.offer(arr[i]);
+        }
+        for (int i = k; i < arr.length; ++i) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = queue.poll();
+        }
+        return vec;
+    }
+}
+
